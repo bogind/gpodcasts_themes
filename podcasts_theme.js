6f718128b67ebe08d792a6c888b68a0ab80acfca
bogind <dror@kaplanopensource.co.il>
2021-06-23 20:03:30 +0300

    basic template and themes themes taken from youtube enhancer and colorpalettes.net

---
+++ podcasts_theme.js
@@ -0,0 +1,55 @@
+let base_themes = {
+    "Blue dark":{
+        "background":"#272b35",
+        "background2":"#181d20",
+        "text":"#e3eef9",
+        "links":"#606376",
+        "icons":"#5675b9"
+    },
+    "Purple Dark":{
+        "background":"#111111",
+        "background2":"#181818",
+        "text":"#eff0f1",
+        "links":"#cccccc",
+        "icons":"#ab47bc"
+    },
+    "Orange Dark":{
+        "background":"#0a0400",
+        "background2":"#0e0702",
+        "text":"#fff9f5",
+        "links":"#ffede1",
+        "icons":"#ff6905"
+    },
+    "Ubuntu Purple":{
+        "background":"#2c071a",
+        "background2":"#430b28",
+        "text":"#f2f1ef",
+        "links":"#e6e5e3",
+        "icons":"#ef7847"
+    },
+    "Ubuntu Gray":{
+        "background":"#312d2a",
+        "background2":"#3d3c38",
+        "text":"#f2f1ef",
+        "links":"#e6e5e3",
+        "icons":"#ef7847"
+    },
+    "Neon 1":{
+        "background":"#00170e",
+        "background2":"#1fb598",
+        "text":"#bdfff4",
+        "links":"#1fb598",
+        "icons":"#00b030"
+    },
+    "Neon 2":{
+        "background":"#10133a",
+        "background2":"#1b96f4",
+        "text":"#ef0195",
+        "links":"#fcdf87",
+        "icons":"#f68741"
+    }
+
+}
+function buildPreview(theme){
+
+}
